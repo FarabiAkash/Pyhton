d3dad673bfbf6df7d3302218ad64faa5bcb7f3f3
Al-Farabi Akash <30338934+FarabiAkash@users.noreply.github.com>
2020-08-27 22:30:35 +0600

    Update Prime_Number.py

--- EverydayCode/Prime_Number.py
+++ EverydayCode/Prime_Number.py
@@ -3,5 +3,5 @@ for i in range(2,max_val):
     for j in range(2, int(i/2)+1):
         if i % j == 0:
             break
-    else:
-        print (i , "is prime")
+        else:
+            print (i , "is prime")
